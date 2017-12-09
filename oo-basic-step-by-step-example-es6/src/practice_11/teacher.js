205a12e0af5f756a33cb8e684386db7900c3fc03
unique-mj <13035319028@139.com>
2017-12-09 17:39:10 +0800

    修改

--- oo-basic-step-by-step-example-es6/src/practice_11/teacher.js
+++ oo-basic-step-by-step-example-es6/src/practice_11/teacher.js
@@ -1,29 +1,2 @@
-const Person = require("../../src/practice_11/person.js");
-const Class = require("../../src/practice_11/class");
 
-function Teacher(id, name, age, klasses) {
-    let str = "";
-    Person.call(this, id, name, age);
-    let person = new Person(id, name, age);
-    this.klasses = klasses;
-    this.introduce = function () {
-        if (this.klasses === undefined) {
-            return person.introduce() + " I am a Teacher. I teach No Class."
-        }else {
-            let arr = [];
-            for (let data of this.klasses){
-                arr.push(data.number)
-            }
-            return person.introduce() + " I am a Teacher. I teach Class " + arr.join(", ") + "."
-        }
-    };
-    this.introduceWith = function (student) {
-        if (student.klass.number !== this.klass.number) {
-            str = " don't";
-        }
-        return person.introduce() + " I am a Teacher. I" + str + " teach " + student.name + ".";
-    }
-}
-
-module.exports = Teacher;
 
