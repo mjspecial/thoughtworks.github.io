e244529ffc379de9100a5786062d2396d9fa5da8
unique-mj <13035319028@139.com>
2017-12-21 22:29:20 +0800

    116任务卡

--- scoresheet-cli/lib/main.js
+++ scoresheet-cli/lib/main.js
@@ -25,32 +25,32 @@ function main() {
 
 }
 
-//马娟,201514620224,汉,15网络2,数学：100,语文：99,英语：98,编程：97
-//刘林,201514620222,汉,15网络2,数学：90,语文：89,英语：88,编程：87
+//马娟,201514620224,汉,15网络2,数学:100,语文:99,英语:98,编程:97   冒号要与切割的冒号的中英文的输入时相同的
+//刘林,201514620222,汉,15网络2,数学:90,语文:89,英语:88,编程:87
 function addStudent() {
     var infro = require('cli-interact').question("请输入学生信息（格式：姓名, 学号, 民族, 班级, 学科: 成绩, ...），按回车提交：");
     var stuInfro = infro.split(',');
     if(stuInfro.length !==8) {
-        console.log('请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）');
+        console.log('请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）\n\n');
         addStudent();
     }else {
-        let sum = 0
+        let sum = 0;
         for(var i = 4; i < 8; i++) {
-            sum += stuInfro[i].split(',')[1];
+            sum += parseFloat(stuInfro[i].split(':')[1]);
         }
         let student = {};
         student["name"] = stuInfro[0];
         student["id"] = stuInfro[1];
         student["nation"] = stuInfro[2];
         student["grade"] = stuInfro[3];
-        student["math"] = stuInfro[4].split(',')[1];
-        student["chinese"] = stuInfro[5].split(',')[1];
-        student["english"] = stuInfro[6].split(',')[1];
-        student["programme"] = stuInfro[7].split(',')[1];
-        student["total"] = sum;
-        student["averge"] = sum/4;
+        student["math"] = stuInfro[4].split(':')[1];
+        student["chinese"] = stuInfro[5].split(':')[1];
+        student["english"] = stuInfro[6].split(':')[1];
+        student["programme"] = stuInfro[7].split(':')[1];
+        student["total"] = parseFloat(sum);
+        student["averge"] = parseFloat(sum/4);
         allStudent.push(student);
-        console.log("学生" +stuInfro[0]+ "的成绩被添加\n");
+        console.log("学生" +stuInfro[0]+ "的成绩被添加\n\n");
         main();
     }
 
@@ -61,16 +61,25 @@ function printScore() {
     var infromation = require('cli-interact').question("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：");
     var stuNum = infromation.split(',');
     if(isNaN(stuNum[0])) {
-        console.log("请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...）");
+        console.log("请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...）\n\n");
         printScore();
     }else {
         console.log("成绩单\n姓名|数学|语文|英语|编程|平均分|总分");
         console.log("========================");
-        var sum_middle = [];
+        let sum_middle = [];
+        let number ;
         for(var i = 0; i < stuNum.length; i++) {
-            var arr = stuNum[i];
-            let student = allStudent[arr];
-            var str = '';
+            let arr = stuNum[i];
+            for(var j=0; j<allStudent.length; j++) {
+                if(allStudent[j]["id"] == arr) {
+                    number = j;
+                }
+            }
+            if(number == undefined) {
+                break;
+            }
+            let student = allStudent[number];
+            let str = '';
             str = student["name"] +' | '+
                     student["math"] + ' | '+
                     student["chinese"] + ' | '+
@@ -85,19 +94,19 @@ function printScore() {
 
         var sum_total = 0;
         for(var j = 0; j < allStudent.length; j++) {
-            sum_total += allStudent[j].total;
+            sum_total += parseFloat(allStudent[j].total);
         }
         var sum_averge = 0;
-        sum_averge = sum_total/allStudent.length;
+        sum_averge = parseFloat(sum_total/allStudent.length);
         console.log("全班总分平均数：" + sum_averge);
 
         var middle = 0;
         if(sum_middle.length % 2 == 0) {
-             middle = (sum_middle[sum_middle.length / 2] + sum_middle[sum_middle.length / 2 + 1])/2;
+             middle = (parseFloat(sum_middle[sum_middle.length / 2])+ parseFloat(sum_middle[sum_middle.length / 2 - 1]))/2;
         }else {
-            middle = sum_middle[parseInt(sum_middle.length/2)+1];
+            middle = sum_middle[parseInt(sum_middle.length/2)];
         }
-        console.log("全班总分中位数：" + middle);
+        console.log("全班总分中位数：" + middle +'\n');
     }
 
     main();
